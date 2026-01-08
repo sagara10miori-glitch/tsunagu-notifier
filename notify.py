20eb3e7ab4895e2fcf1c791c5f088e4d6ad785cc
sagara10miori-glitch <sagara10miori@gmail.com>
2026-01-09 01:07:18 +0900

    Update notify.py

--- notify.py
+++ notify.py
@@ -118,6 +118,7 @@ def main():
     # -----------------------------
     # データ読み込み
     # -----------------------------
+    print("WEBHOOK_URL:", repr(WEBHOOK_URL))
     last_all = load_json(DATA_LAST_ALL, default={})
     last_special = load_json(DATA_LAST_SPECIAL, default={})
 
@@ -218,5 +219,4 @@ def main():
 # 実行
 # -----------------------------
 if __name__ == "__main__":
-    print("WEBHOOK_URL:", repr(WEBHOOK_URL))
     safe_run(main)
