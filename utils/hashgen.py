e52f0e1398e3067595061c76b5bba42aba7c4102
sagara10miori-glitch <sagara10miori@gmail.com>
2026-01-13 07:57:35 +0900

    Update hashgen.py

--- utils/hashgen.py
+++ utils/hashgen.py
@@ -1,5 +1,4 @@
 import hashlib
 
-def generate_item_hash(title, author, url):
-    base = f"{title}|{author}|{url}"
-    return hashlib.sha256(base.encode("utf-8")).hexdigest()
+def generate_item_hash(url):
+    return hashlib.sha256(url.encode("utf-8")).hexdigest()
