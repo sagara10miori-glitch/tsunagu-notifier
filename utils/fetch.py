3dcf68e5904d9e2b08be24d95aadfc205741f4e6
sagara10miori-glitch <sagara10miori@gmail.com>
2026-01-14 20:09:50 +0900

    Update fetch.py

--- utils/fetch.py
+++ utils/fetch.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 
+
 def parse_html(html: str):
     return BeautifulSoup(html, "html.parser") if html else None
+
 
 def validate_image_url(url: str):
     return url if url and url.startswith(("http://", "https://")) else None
